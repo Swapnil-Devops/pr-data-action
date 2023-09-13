b7fd03c21982f5491ca61f514862761b779431b4
swapnil <swapnilgawali244@gmail.com>
2023-09-13 17:19:21 +0530

    added files for an action

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const { Octokit } = require("@octokit/core");
+const fetch = require("node-fetch"); // Import the fetch implementation
 
-try {
-  const octokit = new Octokit({
-    auth: process.env.INPUT_TOKEN,
-  });
-
-  const response = await octokit.request(
-    "GET /repos/{owner}/{repo}/pulls/{pull_number}/files",
-    {
-      owner: process.env.INPUT_OWNER,
-      repo: process.env.INPUT_REPO,
-      pull_number: process.env.INPUT_PULL_NUMBER,
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
+async function fetchPullRequestFiles() {
+  try {
+    const octokit = new Octokit({
+      auth: process.env.INPUT_TOKEN,
+      request: {
+        fetch, // Pass the fetch implementation here
       },
-    }
-  );
+    });
 
-  const files = response.data;
-  console.log("Fetched files:", files);
+    const response = await octokit.request(
+      "GET /repos/{owner}/{repo}/pulls/{pull_number}/files",
+      {
+        owner: process.env.INPUT_OWNER,
+        repo: process.env.INPUT_REPO,
+        pull_number: process.env.INPUT_PULL_NUMBER,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
 
-  // Set the 'files' output
-  process.stdout.write(JSON.stringify({ files }));
-} catch (error) {
-  console.error("Error:", error);
-  process.exit(1);
+    const files = response.data;
+    console.log("Fetched files:", files);
+
+    // Set the 'files' output
+    process.stdout.write(JSON.stringify({ files }));
+  } catch (error) {
+    console.error("Error:", error);
+    process.exit(1);
+  }
 }
+
+// Call the async function to start execution
+fetchPullRequestFiles();
