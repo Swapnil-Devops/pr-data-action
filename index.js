fdbc9b6ff9f3aa93c7a047dbe125f614fcbe62d8
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-09-13 16:10:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const { Octokit } = require("@octokit/core");
+const { Octokit } = require('@octokit/core');
 
-try {
-  const octokit = new Octokit({
-    auth: process.env.INPUT_TOKEN,
-  });
+async function fetchPullRequestFiles() {
+  try {
+    const octokit = new Octokit({
+      auth: process.env.INPUT_TOKEN
+    });
 
-  const response = await octokit.request(
-    "GET /repos/{owner}/{repo}/pulls/{pull_number}/files",
-    {
+    const response = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
       owner: process.env.INPUT_OWNER,
       repo: process.env.INPUT_REPO,
       pull_number: process.env.INPUT_PULL_NUMBER,
       headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    }
-  );
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
 
-  const files = response.data;
-  console.log("Fetched files:", files);
+    const files = response.data;
+    console.log('Fetched files:', files);
 
-  // Set the 'files' output
-  process.stdout.write(JSON.stringify({ files }));
-} catch (error) {
-  console.error("Error:", error);
-  process.exit(1);
+    // Set the 'files' output
+    process.stdout.write(JSON.stringify({ files }));
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
 }
+
+// Call the async function to start execution
+fetchPullRequestFiles();
