e4cb071be667d0f61ce570b1bba6ca607c912ba4
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-09-15 13:23:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { Octokit } from "@octokit/core";
-import fetch from "node-fetch"; // Import the fetch implementation
+import fetch from "node-fetch";
 import fs from "fs";
 
-async function fetchPullRequestFiles() {
+async function fetchAndProcessFiles() {
   try {
     const octokit = new Octokit({
       auth: process.env.INPUT_TOKEN,
       request: {
-        fetch, // Pass the fetch implementation here
+        fetch,
       },
     });
 
@@ -29,40 +29,36 @@ async function fetchPullRequestFiles() {
     // Define the list of allowed file extensions
     const allowedExtensions = [".js", ".ts", ".py", ".rs", ".cpp", ".cxs", ".hpp"];
 
-    // Filter the files based on the allowed extensions
-    const filteredFiles = files.filter(file => {
+    for (const file of files) {
+      // Check if the file has an allowed extension
       const fileExtension = file.filename.slice(file.filename.lastIndexOf("."));
-      return allowedExtensions.includes(fileExtension);
-    });
+      if (allowedExtensions.includes(fileExtension)) {
+        try {
+          // Fetch the content of the file from GitHub
+          const fileContentResponse = await octokit.request("GET " + file.raw_url);
+          const fileContent = fileContentResponse.data;
 
-    const matchingFilenames = [];
-    for (const file of filteredFiles) {
-      const content = fs.readFileSync(file.filename, "utf-8");
-      const lines = content.split("\n");
-      let firstNonBlankLine = null;
+          // Split the content into lines
+          const lines = fileContent.split("\n");
 
-      for (const line of lines) {
-        const trimmedLine = line.trim();
-        if (trimmedLine.length > 0) {
-          firstNonBlankLine = trimmedLine;
-          break;
+          // Find the first non-blank line containing "generate" and "testcase"
+          const firstMatchingLine = lines.find((line) => {
+            const trimmedLine = line.trim();
+            return trimmedLine.length > 0 && trimmedLine.includes("generate") && trimmedLine.includes("testcase");
+          });
+
+          if (firstMatchingLine) {
+            console.log('File:', file.filename);
+            console.log('First matching line:', firstMatchingLine);
+
+            // Log the entire content of the file
+            console.log('File Content:', fileContent);
+          }
+        } catch (error) {
+          console.error("Error fetching or processing file content:", error);
         }
       }
-
-      // Check if the first non-blank line contains "generate" and "testcase"
-      if (firstNonBlankLine && firstNonBlankLine.includes("generate") && firstNonBlankLine.includes("testcase")) {
-        console.log('File:', file.filename);
-        console.log('First non-blank line:', firstNonBlankLine);
-
-        // Add the filename to the matchingFilenames array
-        matchingFilenames.push(file.filename);
-      }
     }
-
-    // Print the matching filenames
-    console.log('Matching Filenames:', matchingFilenames);
-    // Set the 'files' output
-    // process.stdout.write(JSON.stringify({ files }));
   } catch (error) {
     console.error("Error:", error);
     process.exit(1);
@@ -70,4 +66,4 @@ async function fetchPullRequestFiles() {
 }
 
 // Call the async function to start execution
-fetchPullRequestFiles();
+fetchAndProcessFiles();
