1333d601ef8f5ee64b8529f4577014436b3962f4
sidharth-14 <sidharthtembhare1244@gmail.com>
2023-09-20 19:14:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 import { Octokit } from "@octokit/core";
+import { context } from "@actions/github";
 import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
 import OpenAIAssistant from "./gpt.js";
 
-const generator = new OpenAIAssistant();
+class CodeProcessor {
+  constructor() {
+    this.generator = new OpenAIAssistant();
+  }
 
-async function fetchAndProcessFiles() {
-  try {
+  async processFiles() {
+    try {
+      // const workspaceDirectory = process.env.GITHUB_WORKSPACE;
+      const octokit = new Octokit({
+        auth: process.env.INPUT_TOKEN,
+        request: {
+          fetch,
+        },
+      });
 
-    // Get the GitHub workspace directory
-    const workspaceDirectory = process.env.GITHUB_WORKSPACE;
+      const files = await this.getPullRequestFiles(octokit);
 
-    const octokit = new Octokit({
-      auth: process.env.INPUT_TOKEN,
-      request: {
-        fetch,
-      },
-    });
 
+      for (const file of files) {
+        const fileExtension = path.extname(file.filename);
+        if (this.isFileExtensionAllowed(fileExtension) && file.status !== 'removed') {
+          try {
+            const fileContent = await this.getFileContent(octokit, file.raw_url);
+            const testcases = await this.generateTestCases(fileContent);
+            const validation = await this.generateValidationCode(fileContent, testcases);
+
+            console.log('validation', validation);
+            console.log('path',context.workspace);
+
+            // The following code to write the testcases to a new file if validation is 'True'.
+            if (validation === 'True') {
+              const newFileName = this.generateTestFileName(file.filename, fileExtension);
+              const filepath = `${context.workspace}/${newFileName}`;
+              const fileExists = fs.existsSync(filepath);
+              if (fileExists){
+                fs.appendFileSync(filepath, testcases);
+              }
+              else{
+                fs.writeFileSync(filepath, testcases);
+              }
+              // const newFilePath = path.join(workspaceDirectory, newFileName);
+              // this.writeTestCasesToFile(newFilePath, testcases);
+            }
+          } catch (error) {
+            console.error("Error fetching or processing file content:", error);
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      process.exit(1);
+    }
+  }
+
+  async getPullRequestFiles(octokit) {
     const response = await octokit.request(
       "GET /repos/{owner}/{repo}/pulls/{pull_number}/files",
       {
@@ -30,71 +71,43 @@ async function fetchAndProcessFiles() {
         },
       }
     );
-
-    const files = response.data;
-
-    // Define the list of allowed file extensions
-    const allowedExtensions = [".js", ".ts", ".py", ".rs", ".cpp", ".cxs", ".hpp"];
-
-    for (const file of files) {
-      // Check if the file has an allowed extension
-      const fileExtension = path.extname(file.filename);
-      if (allowedExtensions.includes(fileExtension) && (file.status != 'removed')) {
-
-        try {
-
-          // Fetch the content of the file from GitHub
-          const fileContentResponse = await octokit.request("GET " + file.raw_url);
-
-          let fileContent = fileContentResponse.data;
-
-          // Split the content into lines
-          const lines = fileContent.split("\n");
-
-          // Find the first non-blank line containing "generate" and "testcase"
-          const firstMatchingLine = lines.find((line) => {
-            const trimmedLine = line.trim();
-            return trimmedLine.length > 0 && trimmedLine.includes("generate") && trimmedLine.includes("testcase");
-          });
-
-          if (firstMatchingLine) {
-            // Log the entire content of the file
-            // console.log('File Content:', fileContent);
-            let fileContents = 'I want you to act like a senior testcase code developer. I will give you code, and you will write the testcases. Do not provide any explanations. Do not respond with anything except of the code. Also include import packages in the code. The code is:' + fileContent;
-
-            const testcases = await generator.generate(fileContents);
-            console.log('testcasese:',testcases);
-
-            let testcasevalidation = fileContent + 'This is the code.' + testcases + 'This are the testcases for the code. Reply as "True" if all test cases pass and "False" even if the one the testcases fails.  Do not provide any explanations. Do not respond with anything except the true or false.';
-
-            const validation = await generator.generate(testcasevalidation);
-
-            console.log('valdation', validation);
-
-            if (validation == 'True') {
-              // Name the file with a ".test" suffix
-              const newFileName = file.filename.replace(fileExtension, ".test" + fileExtension);
-              console.log('filename:', newFileName);
-
-              // Define the target path within the workspace
-              const newFilePath = path.join(workspaceDirectory, newFileName);
-
-              // Write the testcases data to the new file
-              fs.writeFileSync(newFilePath, testcases);
-              console.log('created testcase file successfully.');
-            }
-
-          }
-        } catch (error) {
-          console.error("Error fetching or processing file content:", error);
-        }
-      }
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    process.exit(1);
+    return response.data;
   }
+
+  isFileExtensionAllowed(fileExtension) {
+    const allowedExtensions = [".js", ".ts", ".py", ".rs", ".cpp", ".cxs", ".hpp"];
+    return allowedExtensions.includes(fileExtension);
+  }
+
+  async getFileContent(octokit, rawUrl) {
+    const fileContentResponse = await octokit.request("GET " + rawUrl);
+    return fileContentResponse.data;
+  }
+
+  async generateTestCases(fileContent) {
+    const fileContents = 'I want you to act like a senior testcase code developer. I will give you code, and you will write the testcases. Do not provide any explanations. Do not respond with anything except of the code. Also include import packages in the code. The code is:' + fileContent;
+    return this.generator.generate(fileContents);
+  }
+
+  async generateValidationCode(fileContent, testcases) {
+    const testcasevalidation = fileContent + 'This is the code.' + testcases + 'This are the testcases for the code. Reply as "True" if all test cases pass and "False" even if the one the testcases fails.  Do not provide any explanations. Do not respond with anything except the true or false.';
+    return this.generator.generate(testcasevalidation);
+  }
+
+  generateTestFileName(originalFileName, fileExtension) {
+    return originalFileName.replace(fileExtension, ".test" + fileExtension);
+  }
+
+  // writeTestCasesToFile(filePath, testcases) {
+  //   fs.writeFileSync(filePath, testcases);
+  //   console.log('Created testcase file successfully.');
+  // }
+}
+
+async function main() {
+  const codeProcessor = new CodeProcessor();
+  await codeProcessor.processFiles();
 }
 
 // Call the async function to start execution
-fetchAndProcessFiles();
+main();
