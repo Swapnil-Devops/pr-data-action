d9ddbd031f0eb12db69c55de33f55e1301942b76
sidharth-14 <sidharthtembhare1244@gmail.com>
2023-09-22 15:59:20 +0530

    Update prExecutor.js

--- prExecutor.js
+++ prExecutor.js
@@ -98,13 +98,13 @@ class PullRequestProcessor {
     }
 
     async generateTestCases(fileContent, filename) {
-        const fileContents = `I want you to act like a senior testcase code developer. I will give you code, and you will write the testcases. Do not provide any explanations. Do not respond with anything except the code. Also include import packages in the code. Give me the complete testcase code file. The name of the file which has code is ${filename}. The code is:\n${fileContent}`;
+        const fileContents = 'What is github?'
         // console.log('gpt prompt',fileContents);
         return this.generator.generate(fileContents);
     }
 
     async generateValidationCode(fileContent, testcases) {
-        const testcasevalidation = `${fileContent} This is the code.\n${testcases} These are the testcases for the code. Validate those and return "true" if testcases are passed and return "false" if any testcase fails. Do not provide any explanations. Do not respond with anything except "true" or "false".`;
+        const testcasevalidation = 'WHat is github actions'
         // console.log('testcase prompt',testcasevalidation);
         return this.generator.generate(testcasevalidation);
     }
