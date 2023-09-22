e3795378e47f4a0ea1764023bd1d20778e65887f
sidharth-14 <sidharthtembhare1244@gmail.com>
2023-09-22 16:04:42 +0530

    changes

--- fullfillmet/gpt.js
+++ fullfillmet/gpt.js
@@ -36,6 +36,7 @@ class OpenAIAssistant {
 
       // Parse the API response as JSON
       const responseData = await response.json();
+      console.log('AI Response',responseData);
 
       if (Array.isArray(responseData.choices) && responseData.choices.length > 0) {
         assistantReply = responseData.choices[0].message.content;
