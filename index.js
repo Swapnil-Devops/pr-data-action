00af282787c9e5706199598f869b6e7116ec746c
sidharth-14 <sidharthtembhare1244@gmail.com>
2023-09-29 10:32:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-import * as pr_executor_1 from "./pr-executor";
-
+var pr_executor_1 = require("./pr-executor");
 function main() {
     return __awaiter(this, void 0, void 0, function () {
         var prProcessor, error_1;
